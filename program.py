2315b8a155c880e9fb7e81517c11301e06ea5bf8
abcde456 <150965674+abcde456@users.noreply.github.com>
2024-11-23 13:21:56 -0500

    Update program.py

--- program.py
+++ program.py
@@ -2,6 +2,8 @@ import customtkinter as ct
 from tkinter import *
 from PIL import Image
 
+semester = "1"
+
 def open(sub):
     pass
 
@@ -35,13 +37,17 @@ ecButton.grid(row=1, column=0, sticky=E+W, pady=10)
 tutorButton = ct.CTkButton(sideFrame, text="Tutors", command=tutorTab, fg_color="#0b172b", bg_color="#FFFFFF", height=152)
 tutorButton.grid(row=2, column=0, sticky=E+W, pady=10)
 
+eButton = ct.CTkButton(sideFrame, text="e", command=tutorTab, fg_color="#0b172b", bg_color="#FFFFFF", height=152)
+eButton.grid(row=4, column=0, sticky=E+W, pady=10)
 
 
 title = ct.CTkLabel(app, text="Subjects", font=h1Font, width=1076)
 title.grid(row=0, column=1, columnspan=4, sticky=E)
 
-sem1Title = ct.CTkLabel(app, text="Semester 1:", font=h2Font)
-sem1Title.grid(row=1, column=1, pady=20)
+
+
+semTitle = ct.CTkLabel(app, text="Semester "+semester+":", font=h2Font)
+semTitle.grid(row=1, column=1, pady=20)
 
 sub1Button = ct.CTkButton(app, text="Math", font=h2Font, command=open(sub = "math"), width=150, height=150)
 sub1Button.grid(row=2, column=1)
@@ -54,9 +60,6 @@ sub3Button.grid(row=2, column=3)
 
 sub4Button = ct.CTkButton(app, text="Tech", font=h2Font, command=open(sub = "tech"), width=150, height=150)
 sub4Button.grid(row=2, column=4)
-
-sem2Title = ct.CTkLabel(app, text="Semester 2:", font=h2Font)
-sem2Title.grid(row=3, column=1, pady=20)
 
 sub5Button = ct.CTkButton(app, text="Math", font=h2Font, command=open(sub = "math"), width=150, height=150)
 sub5Button.grid(row=4, column=1)
